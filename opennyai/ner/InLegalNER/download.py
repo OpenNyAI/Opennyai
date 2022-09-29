21256613b0301de96c92dc29fbaf9905d919e175
Aman Tiwari <amant5186@gmail.com>
2022-09-30 01:08:21 +0530

    Refactoring

--- opennyai/ner/InLegalNER/download.py
+++ opennyai/ner/InLegalNER/download.py
@@ -3,7 +3,8 @@ models_url = {
     "en_legal_ner_trf": "https://huggingface.co/opennyaiorg/en_legal_ner_trf/resolve/main/en_legal_ner_trf-any-py3-none-any.whl",
     "en_legal_ner_sm": "https://huggingface.co/opennyaiorg/en_legal_ner_sm/resolve/main/en_legal_ner_sm-any-py3-none-any.whl",
     "en_core_web_sm": "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.2.0/en_core_web_sm-3.2.0-py3-none-any.whl"
-    }
+}
+
 
 def install(package):
     import subprocess
