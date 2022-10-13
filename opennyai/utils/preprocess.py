0d488986afcbc7ef69a87538838b606248c687b7
Aman Tiwari <amant5186@gmail.com>
2022-10-13 09:49:12 +0530

    add some useful dunder function to data class for better usability

--- opennyai/utils/preprocess.py
+++ opennyai/utils/preprocess.py
@@ -9,15 +9,15 @@ from .sentencizer import split_main_judgement_to_preamble_and_judgement
 class Data:
     def __init__(self, input_text, preprocessing_nlp_model='en_core_web_trf', mini_batch_size=40000, use_gpu=True,
                  use_cache=True, verbose=False):
-        self._input_text = input_text
-        self._mini_batch_size = mini_batch_size
-        self._verbose = verbose
-        self._use_cache = use_cache
-        if self._use_cache:
-            self._cache = {}
-        if type(self._input_text) == str:
-            self._input_text = [self._input_text]
-        elif type(self._input_text) == list and len(self._input_text) >= 1:
+        self.__input_text__ = input_text
+        self.__mini_batch_size__ = mini_batch_size
+        self.__verbose__ = verbose
+        self.__use_cache__ = use_cache
+        if self.__use_cache__:
+            self.__cache__ = {}
+        if isinstance(self.__input_text__, str):
+            self.__input_text__ = [self.__input_text__]
+        elif isinstance(self.__input_text__, list) and len(self.__input_text__) >= 1:
             pass
         else:
             raise RuntimeError('No input or wrong given, we accept input as string or list of strings')
@@ -51,32 +51,32 @@ class Data:
                 f'There was an error while loading en_core_web_sm\n To rectify try running:\n pip install -U {PIP_INSTALLER_URLS[preprocessing_nlp_model]}')
 
     def _clean_cache(self):
-        ids = [sha256(text.encode('utf-8')).hexdigest() for text in self._input_text]
-        for key in self._cache.keys():
+        ids = [sha256(text.encode('utf-8')).hexdigest() for text in self.__input_text__]
+        for key in self.__cache__.keys():
             if str(key) not in ids:
-                self._cache.pop(str(key))
+                self.__cache__.pop(str(key))
 
     def __getitem__(self, item):
-        if self._use_cache:
+        if self.__use_cache__:
             self._clean_cache()
-        to_process = self._input_text[item]
-        if type(to_process) == str:
+        to_process = self.__input_text__[item]
+        if isinstance(to_process, str):
             to_process = [to_process]
         data = []
-        if self._verbose:
+        if self.__verbose__:
             msg.info('Processing input data!!!')
-        for text in tqdm(to_process, disable=not self._verbose):
+        for text in tqdm(to_process, disable=not self.__verbose__):
             file_id = sha256(text.encode('utf-8')).hexdigest()
-            if self._use_cache and self._cache.get(file_id) is not None:
-                data.append(self._cache[file_id])
+            if self.__use_cache__ and self.__cache__.get(file_id) is not None:
+                data.append(self.__cache__[file_id])
             else:
                 preamble_doc, judgement_doc = split_main_judgement_to_preamble_and_judgement(text=text,
                                                                                              sentence_splitting_nlp=self.__preprocessing_nlp__,
-                                                                                             mini_batch_size=self._mini_batch_size)
+                                                                                             mini_batch_size=self.__mini_batch_size__)
                 _processed_data = {'file_id': file_id, "preamble_doc": preamble_doc, "judgement_doc": judgement_doc,
                                    "original_text": text}
-                if self._use_cache:
-                    self._cache[file_id] = _processed_data
+                if self.__use_cache__:
+                    self.__cache__[file_id] = _processed_data
                 data.append(_processed_data)
         if len(data) == 1:
             return data[0]
@@ -84,10 +84,28 @@ class Data:
             return data
 
     def __len__(self):
-        return len(self._input_text)
+        return len(self.__input_text__)
 
-    def __call__(self):
-        if len(self._input_text) == 1:
-            return self._input_text[0]
+    def __str__(self):
+        if len(self.__input_text__) == 1:
+            return self.__input_text__[0]
         else:
-            return self._input_text
+            return str(self.__input_text__)
+
+    def __contains__(self, item):
+        return True if item in self.__input_text__ else False
+
+    def __add__(self, other):
+        if isinstance(other, str):
+            self.__input_text__.append(other)
+        else:
+            TypeError('Only str object can be added')
+
+    def append(self, other):
+        self.__add__(other)
+
+    def pop(self, index):
+        if isinstance(index, int):
+            self.__input_text__.pop(index)
+        else:
+            TypeError("'str' object cannot be interpreted as an integer")
