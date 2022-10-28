0e148405e459b7cdc408a5c372c422ec7a934c15
astha-agarwal99 <astha99agarwal@gmail.com>
2022-10-28 20:05:03 +0530

    Add:Helper function for getting unique statutes and provisions

--- opennyai/ner/__init__.py
+++ opennyai/ner/__init__.py
@@ -1,1 +1,1 @@
-from .ner_utils import load, get_json_from_spacy_doc, ner_displacy_option,get_csv
+from .ner_utils import load, get_json_from_spacy_doc, ner_displacy_option,get_csv,get_unique_provision_and_statutes
