b36c104c570fec6accb6d95a92c822df8f416eaa
Aman Tiwari <amant5186@gmail.com>
2022-11-07 13:31:37 +0530

    reset entities ids while loading and testing result of models

--- tests/utils.py
+++ tests/utils.py
@@ -2,4 +2,7 @@ def reset_ids(input_json):
     """ As in result json each annotation has randon id lets set them to a common for testing purpose"""
     for i in input_json['annotations']:
         i['id'] = '1234'
+        if i.get('entities') is not None:
+            for j in i['entities']:
+                j['id'] = '1234'
     return input_json
